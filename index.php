08f1b60eae7cfef410f384e935ee50c72bf5bc90
Gabriele <Gabriele_Virga@hotmail.com>
2017-08-21 13:57:43 +0200

    index fait

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<iframe src="0.form-medical-information/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="1.footer-social/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="2.pagination/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="3.zone-benefits-grid/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="4.zone-cards/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="5.zone-CTA/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="6.zone-hero/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="8.zone-related-links/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="9.zone-story-img+text/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="10.custom-numeric-counters/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="11.ecommerce-catalog/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+	<iframe src="12.ui-chart/index.html" frameborder="10" height="500px" width="1000px"></iframe>
+</body>
+</html>
